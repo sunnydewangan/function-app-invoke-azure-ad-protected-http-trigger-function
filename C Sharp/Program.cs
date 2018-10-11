b47e98d43503dc360fbb240b8b0837ad0bd5d036
Sunny Dewangan <sadewa@microsoft.com>
2018-10-11 21:15:34 +0530

    Initial Commit

--- C Sharp/Program.cs
+++ C Sharp/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace C_Sharp
 {
@@ -10,6 +9,45 @@ namespace C_Sharp
     {
         static void Main(string[] args)
         {
+            string _tenantID = "{Tenant ID}"; //Active Directory ID
+            string _applicationID = "{Client Application ID}"; // Your AD Application ID a.k.s Client ID
+            string _applicationSecret = "{Client Application Secret}"; // Your AD Application Secret a.k.s. Client Secret
+
+            string _accessToken = CreateAccessToken(_tenantID, _applicationID, _applicationSecret);
+            _accessToken = "Bearer " + _accessToken;
+
+            string _result = HttpGet("{Your HttpTrigger Function URL with code}", _accessToken);
+
+            Console.WriteLine(_result);
+            Console.ReadLine();
+        }
+        public static string CreateAccessToken(string TenantID, string ApplicationID, string ApplicationSecret)
+        {
+            try
+            {
+                var context = new AuthenticationContext("https://login.microsoftonline.com/" + TenantID + "/oauth2/token");
+                ClientCredential clientCredential = new ClientCredential(ApplicationID, ApplicationSecret);
+                var tokenResponse = context.AcquireTokenAsync(ApplicationID, clientCredential).Result;
+                var accessToken = tokenResponse.AccessToken;
+                return accessToken;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public static string HttpGet(string url, string accessToken)
+        {
+            WebHeaderCollection headers = new WebHeaderCollection
+            {
+                { "Authorization", accessToken }
+            };
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Headers = headers;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            return sr.ReadToEnd();
         }
     }
 }
---
+++ PowerShell/PowerShell.ps1
@@ -0,0 +1,26 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$clientId = '{Client ID}' # Azure AD Client ID
+$clientSecret = '{Client Secret}' #Azure AD Client Secret
+$tokenEndpoint = 'https://login.microsoftonline.com/{TanantID}/oauth2/token'  #AD Endpoint where we request for token
+
+$body = @{
+    resource = $clientId
+    client_id = $clientId
+    client_secret = $clientSecret
+    grant_type = "client_credentials"
+}
+
+# Making a POST call to get Token
+$result = Invoke-RestMethod -Method Post -Uri $tokenEndpoint -Body $body
+$token = $result.access_token
+Write-Information "Token: $token"
+
+$urlToAccess = "{Your HttpTrigger Function URL with Code}"
+
+# Invoke HttpTrigger Function while passing access token
+$response = Invoke-WebRequest -Uri $urlToAccess -Method "GET" -Headers @{ "Authorization" = "Bearer " + $token }
+
+Write-Information ("Status code: {0}`r`nBody: {1}" -f $response.StatusCode, $response.Content)
+
+#Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear(); Clear-Host
